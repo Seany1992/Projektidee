48dc4e852e0f6cda784fcb890d992f34792da30e
Seany1992 <s.gocks@live.de>
2016-08-25 09:28:49 +0200

    Fragen an Herrn Alpers
    Frage neu formuliert, bitte um eine Antwort

--- Gruppen_de/php_code.php
+++ Gruppen_de/php_code.php
@@ -1,14 +1,10 @@
 <?php
 /*
 FRAGEN, HINWEISE und ANMERKUNGEN zum Aufgabenteil PHP an Herrn Alpers:
-1. Aus der Aufgabenstellung heraus, wird mir nicht 100% ersichtlich, wie ich diesen Aufabenteil, also die Einbindung von PHP in HTML Dateien, strukturiere.
+1. Aus der Aufgabenstellung heraus, wird mir nicht 100% ersichtlich, wie ich diesen Aufabenteil, also die Einbindung von PHP in HTML Dateien, strukturiere?
 
-	- Bisher habe ich reine HTML Dokumente  im Verzeichnis "Gruppen_de" erstellt, sogesagt die Prototyp Datei ohne PHP. Auf der anderen Seite habe ich ein Verzeichnis "Formulare" mit den PHP Datein der einzelnen Formulare.
-	
-	Zurzeit ist meine Vorgehensweise folgende: Im Verzeichnis "Formulare" befinden sich die indentischen HTML Dokumente, welche auch im Gruppen_de Verzeichnis vorzufinden sind, nur mit dem Zusatz der PHP Einbindung. Somit ist der "alte" ursprüngliche Code immernoch einzusehen, und es wird klar ersichtlich wo Aufgabenblock, (3) Erste Schritte hin zu PHP, beginnt. Eventuell für Sie wichtig für die Punktevergabe der einzelnen Aufgabenteile?
-	
-	Ist diese Vorgehensweise in Ordnung? 
-	Oder soll es am Ende darauf hinauslaufen, dass sich keine HTML Dokumente mehr im Repository vorfinden lassen? Sprich die HTML Datein, sind durch richtige  Einbindung von PHP allesamt zu .php Datein geworden?
+	- Wird die PHP Einbindung, anhand des erstellten Verzeichnises "Formulare" und den enthaltenen Datein "form_eva_gruppenname", ausgelagert? Sprich findet die PHP Programmierungen in den neu erstellten Datein statt? Oder kann ich auch einfach in den bereits bestehenden HTML datein meinen php Code einbinden? (siehe quiz.php)
+	Dazu muss ich noch sagen, dass ich erst gerade mit meinem PHP Teil angefangen habe und mein Wissen bisher auf die Grundlagen beschränkt ist.
 	
 	Vielen Dank im Voraus.
 
