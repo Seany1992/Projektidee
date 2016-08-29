2e3747ce987b582bc946fa52b67f2677be5fec1e
Seany1992 <s.gocks@live.de>
2016-08-29 11:47:05 +0200

    update
    Verlinkugnen angepasst, Frage entfernt

--- Gruppen_de/php_code.php
+++ Gruppen_de/php_code.php
@@ -1,12 +1,7 @@
 <?php
 /*
 FRAGEN, HINWEISE und ANMERKUNGEN zum Aufgabenteil PHP an Herrn Alpers:
-1. Aus der Aufgabenstellung heraus, wird mir nicht 100% ersichtlich, wie ich diesen Aufabenteil, also die Einbindung von PHP in HTML Dateien, strukturiere?
 
-	- Wird die PHP Einbindung, anhand des erstellten Verzeichnises "Formulare" und den enthaltenen Datein "form_eva_gruppenname", ausgelagert? Sprich findet die PHP Programmierungen in den neu erstellten Datein statt? Oder kann ich auch einfach in den bereits bestehenden HTML datein meinen php Code einbinden? (siehe quiz.php)
-	Dazu muss ich noch sagen, dass ich erst gerade mit meinem PHP Teil angefangen habe und mein Wissen bisher auf die Grundlagen beschränkt ist.
-	
-	Vielen Dank im Voraus.
 
 */
 ?>
