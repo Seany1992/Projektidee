fd0a92cc509cb83cd44d940883430e11dbe23543
Seany1992 <s.gocks@live.de>
2016-09-05 10:50:27 +0200

    Frage an Herrn Alpers
    Frage hinzugefügt

--- Gruppen_de/php_code.php
+++ Gruppen_de/php_code.php
@@ -1,7 +1,8 @@
 <?php
 /*
 FRAGEN, HINWEISE und ANMERKUNGEN zum Aufgabenteil PHP an Herrn Alpers:
-
+1. 
+Damit eine form_eval_grupenname.php Datei auf ein HTML DOkument zugreifen kann, müssen sie sich im selben Verzeichnis befinden. Wie ordne ich am sinnvollsten die HTML sowie die erstellten PHP Datein im Repository?
 
 */
 ?>
