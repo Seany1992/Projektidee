12c43bf612cbc4e483740fe54cd7e626344618f8
Seany1992 <s.gocks@live.de>
2016-10-04 23:34:02 +0200

    Duellmodus als Ansicht
    Duellmodus als einzelne Ansicht dem Wurzelverzeichnis hinzugefügt,
    enthält die Include Option für die Gruppe duell.php

---
+++ duellmodus.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang=de> 
+	<head>
+		<meta charset=utf-8> 
+		<title>Quizprojekt</title> 
+	<body>
+		<header>
+			<img><!-- Hier wird das Logo des Quizes eingefügt --></img>
+			<h1>Duellmodus</h1>	
+		</header>
+		
+		<?php include("Gruppen_de/duell.php") ?>
+		
+	</body>
+</html>
