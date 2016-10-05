2a923d94643e2e8a4ee9a2a5b7ab617f5296f847
Seany1992 <s.gocks@live.de>
2016-10-05 23:45:40 +0200

    Alpers feedback hinzugefügt
    ausversehen gelöscht, nun wieder hinzugefügt

--- Formulare/form_eval_login.php
+++ Formulare/form_eval_login.php
@@ -1,4 +1,4 @@
-<!-- Hier werden Eingaben aus login.php verarbeitet -->
+﻿<!-- Hier werden Eingaben aus login.php verarbeitet -->
 	<?php
 	echo "Vielen Dank für die Registrierung: ";
 	echo htmlspecialchars ($_POST["username"]);
@@ -7,4 +7,12 @@
 	echo htmlspecialchars ($_POST["email"]);
 	echo "<br>";
 	echo "<a href=../hub.php> <button type=button id=hauptmenue>Hauptmenü</button></a>";
+	
+	// ===================================
+	// === Feedback Alpers, 2016-10-04 ===
+	// ===================================
+	//
+	// Diese Datei ist eine PHP-Datei zur Auswertung eines Formulars, kein HTML-Dokument.
+	// Umgesetzt haben Sie dagegen ein HTML-Formular.
 	?>
+
