df1cf8b4a867544c59dfccfd476a791788c8c2da
Seany1992 <s.gocks@live.de>
2016-10-05 23:08:52 +0200

    Ansicht adminbereich hinzugefügt
    Die Ansicht adminbereich.php hinzugefügt mit inlucde optionen füer
    quizfragen.php und feedbacktabelle.php

---
+++ adminbereich.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang=de> 
+	<head>
+		<meta charset=utf-8> 
+		<title>Quizprojekt</title> 
+	</head>
+
+	<header>
+			<img><!-- Hier wird das Logo des Quizes eingefügt --></img>
+			<h1>Adminbereich</h1> 
+	</header>
+	
+	<body>
+		<section>
+			<?php include("Gruppen_de/feedbacktabelle.php") ?>	
+		
+			<button type=button id=feedback >Feedback beantworten</button> 
+			<!-- Funktion: Der Admin wird bei Betätigung auf ausgewählte Feedbacks antworten können. --> 
+			 <br>
+								
+			<button type=button id=löschen>Feedback löschen</button> 
+			<!-- Funktion: Der Admin wird bei Betätigung ausgewählte Feedbacks löschen können. -->
+		</section>	
+		
+		<section>
+			<?php include("Gruppen_de/quizfragen.php") ?>	
+		</section>
+	</body>
+</html>
