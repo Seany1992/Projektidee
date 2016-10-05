0168a52326bdbbe458bfe31380022d810510e09d
Seany1992 <s.gocks@live.de>
2016-10-05 16:10:59 +0200

    Gruppen erstellt
    Gruppen für die Ansicht Endabrechnung erstellt

--- Gruppen_de/auswertung.php
+++ Gruppen_de/auswertung.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang=de> 
-	<head>
-		<meta charset=utf-8> 
-		<title>Quizprojekt</title> 
-	</head>
 
-	<body>
-		<header>
-			<h1>Auswertung</h1>	
-		</header>	
-		<main>
 		<?php
 			//Info: Die Anzahl der richtig beantworteten Fragen und die Erfolgsquote beim Quiz soll hier angezeigt werden (Bezeichner "x" sind momentan noch Platzhalter)"
 			$richtigeAntworten = 15;
@@ -32,25 +21,3 @@
 			echo "<p> Ihre Erfolgsquote liegt bei: $ergebnis % <p>" ;
 			// In diesem Beispiel wurden 15 von 20 möglichen Fragen richtig beantwortet. im fertigen Quiz, wird die Variable für die richtigen Antworten vom Quizteil übergeben. 
 			?>
-			<form method=post action="<?php echo $_SERVER["PHP_SELF"]; ?>" >
-			<?php
-			$array[] = array("Platzierung: ", "Spielername: ", "Highscore: ");
-			$array[] = array("Platz1", "Spieler1", "Zahlenwert1");
-			$array[] = array("Platz2", "Spieler2", "Zahlenwert2");
-			echo "<table>";
-			foreach($array as $row) 
-			{
-				echo "<tr>";
-				echo "<td>" . implode("</td><td>", $row) . "</td>";
-				echo "</tr>";
-			}
-				echo "</table>";
-				
-				//Info: Die erzeugte Tabelle mithilfe von PHP ist nicht vollständig (es handelt sich bei den Bezeichnern noch um Platzhalter), sie wird nach und nach mit Nutzern aus der Datenbank gefüllt. Es handelt sich um die Highscore Liste, dort aufgelistet werden alle gespeicherten Nutzerprofile mitsamt ihrem Highscore 
-			?>
-			</form>
-				<a href=quiz.php> <button type=button id=start>Neustart</button></a>
-				<a href=hauptmenue.html> <button type=button id=hauptmenue>Hauptmenü</button></a>
-		</main>	
-	</body>
-</html>
---
+++ Gruppen_de/highscoreliste.php
@@ -0,0 +1,19 @@
+<form method=post action="<?php echo $_SERVER["PHP_SELF"]; ?>" >
+			<?php
+			$array[] = array("Platzierung: ", "Spielername: ", "Highscore: ");
+			$array[] = array("Platz1", "Spieler1", "Zahlenwert1");
+			$array[] = array("Platz2", "Spieler2", "Zahlenwert2");
+			echo "<table>";
+			foreach($array as $row) 
+			{
+				echo "<tr>";
+				echo "<td>" . implode("</td><td>", $row) . "</td>";
+				echo "</tr>";
+			}
+				echo "</table>";
+				
+				//Info: Die erzeugte Tabelle mithilfe von PHP ist nicht vollständig (es handelt sich bei den Bezeichnern noch um Platzhalter), sie wird nach und nach mit Nutzern aus der Datenbank gefüllt. Es handelt sich um die Highscore Liste, dort aufgelistet werden alle gespeicherten Nutzerprofile mitsamt ihrem Highscore 
+			?>
+			</form>
+				<a href=quizseite.php> <button type=button id=start>Neustart</button></a>
+				<a href=hub.php> <button type=button id=hauptmenue>Hauptmenü</button></a>	
