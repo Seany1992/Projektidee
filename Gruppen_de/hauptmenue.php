81a2a787dee188815f2fcaf246a082a74223f628
Seany1992 <s.gocks@live.de>
2016-10-05 15:52:21 +0200

    gruppe hauptmenue hinzugefügt
    Die Gruppe hauptmenue.php hinzugefügt

---
+++ Gruppen_de/hauptmenue.php
@@ -0,0 +1,15 @@
+
+	<a href=quiz.php> <button type=button id=einzelspieler>Einzelspieler</button></a> 
+	 <br>
+							
+	<a href=trainingsmodus.php> <button type=button id=trainingsmodus>Trainingsmodus</button></a>
+	 <br>
+							
+	<a href=duellmodus.php> <button type=button id=duellmodus>Duellmodus</button></a>
+	 <br>
+							
+	<a href=userforum.php> <button type=button id=userforum>Userforum</button></a> 
+	 <br>
+							
+	<a href=adminbereich.php> <button type=button id=quizfragen>Quizfragen bearbeiten</button></a>		
+		
