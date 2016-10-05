7fce44d4a3c925fd19fdfbf097fbd9eaa376b60b
Seany1992 <s.gocks@live.de>
2016-10-05 16:10:31 +0200

    Ansicht Endabrechnung hinzugefügt
    Ansicht Endabrechnung hinzugefügt, include option auswertung.php und
    highscoreliste.php

---
+++ Endabrechnung.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang=de> 
+	<head>
+		<meta charset=utf-8> 
+		<title>Quizprojekt</title> 
+	</head>
+
+	<body>
+		<header>
+			<h1>Auswertung</h1>	
+		</header>	
+		<?php include("Gruppen_de/auswertung.php") ?>
+		
+		<?php include("Gruppen_de/highscoreliste.php") ?>
+		
+	</body>
+</html>
