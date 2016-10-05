d27e4814f52fdc329798016c640bd2a56366d2a2
Seany1992 <s.gocks@live.de>
2016-10-05 15:33:09 +0200

    Gruppe quiz ergänzt um PHP
    PHP einbindung für Versuche Zeitlimit udn Punktzahl eingefügt,
    formatierung angepasst

--- Gruppen_de/quiz.php
+++ Gruppen_de/quiz.php
@@ -1,51 +1,56 @@
 <p>Frage 1 von X</p> 
-		<!--Funktion: Wird eine fortlaufende Nummerierung der Fragen z.b. "1"-->
+	<!--Funktion: Wird eine fortlaufende Nummerierung der Fragen z.b. "1"-->
 		
-		
+<form action=form_eval_quiz.php method=POST>
+	<fieldset>
+		<legend>Quizfrage</legend> 
 		<!-- Funktion: Momentan noch Platzhalter. Hier wird die Frage eingefügt -->
-		<form action=form_eval_quiz.php method=POST>
-			<fieldset>
-					<legend>Quizfrage</legend> 
 					
-					<label for=antwort1> A: Antwort</label> 
-					<input type=radio id=antwort1 name=antwort value=antwort1 required> 
-					<br>
-					<label for=antwort2> B: Antwort</label> 
-					<input type=radio id=antwort2 name=antwort value=antwort2 required>
-					<br>
-					<label for=antwort3> C: Antwort</label> 
-					<input type=radio id=antwort3 name=antwort value=antwort3 required>
-					<br>
-					<label for=antwort4> D: Antwort</label>
-					<input type=radio id=antwort4 name=antwort value=antwort4 required>	
-			</fieldset>	
-		</form>
-			<img><!--Hier Themenspezifische EInblendung von Hintergrundbildern--></img>
-			<a href=quiz.php> <button type=button id=previous>Vorherige Frage</button> </a>
+		<label for=antwort1> A: Antwort</label> 
+		<input type=radio id=antwort1 name=antwort value=antwort1 required> 
+		 <br>
+		<label for=antwort2> B: Antwort</label> 
+		<input type=radio id=antwort2 name=antwort value=antwort2 required>
+		 <br>
+		<label for=antwort3> C: Antwort</label> 
+		<input type=radio id=antwort3 name=antwort value=antwort3 required>
+		 <br>
+		<label for=antwort4> D: Antwort</label>
+		<input type=radio id=antwort4 name=antwort value=antwort4 required>	
+	</fieldset>	
+</form>
+	<img><!--Hier Themenspezifische EInblendung von Hintergrundbildern--></img>
+	<a href=quiz.php> <button type=button id=previous>Vorherige Frage</button> </a>
 			
-			<a href=quiz.php> <button type=button id=skip>Nächste Frage</button></a>
+	<a href=quiz.php> <button type=button id=skip>Nächste Frage</button></a>
 			
-			<a href=auswertung.php> <button type=button id=auswertung>Zur Auswertung</button></a>
+	<a href=auswertung.php> <button type=button id=auswertung>Zur Auswertung</button></a>
 			
-			<a href=regelwerk.txt> <button type=button id=regelwerk>Regelwerk aufrufen</button></a>
-		<aside>
-			<!-- Info: Hier soll eine Liste mit dem Fortschritt innerhalb des Quizes folgen. Es dient als Orientierungsliste für die Spielrunde. Angezeigt wird die Anzahl der noch offenen- und bereits richtig beantworteten Fragen. Die aktuelle Frage, bei der man sich befindet, wird gekennzeichnet.  -->
-				<?php
-				echo "<table border=1>\n";
-				for ($i = 1; $i <= 20; $i++) 
-				{ 
-					echo "<td>Frage: $i</td>\n";
-					for ($j=1; $j <= 1; $j++)
-					{
-						echo "<tr>\n";
-					}
+	<a href=regelwerk.txt> <button type=button id=regelwerk>Regelwerk aufrufen</button></a>
+	<aside>
+		<!-- Info: Hier soll eine Liste mit dem Fortschritt innerhalb des Quizes folgen. Es dient als Orientierungsliste für die Spielrunde. Angezeigt wird die Anzahl der noch offenen- und bereits richtig beantworteten Fragen. Die aktuelle Frage, bei der man sich befindet, wird gekennzeichnet.  -->
+		<?php
+			echo "<table border=1>\n";
+			for ($i = 1; $i <= 20; $i++) 
+			{ 
+				echo "<td>Frage: $i</td>\n";
+				for ($j=1; $j <= 1; $j++)
+				{
+					echo "<tr>\n";
 				}
-				echo "</table>\n";
-				/* Fortschrittsleiste mithilfe von PHP realisiert, es wird eine Tabelle mit den Fragen von 1-20 angezeigt.
-				*/
-				?>
-		</aside>
-		 <!-- Info: Bereich, um die Lebensanzeige, das Zeitlimit und die erreichte Punktzahl dynamisch darzustellen, mithilfe von Skripten/Funktionen in PHP -->
+			}
+			echo "</table>\n";
+			/* Fortschrittsleiste mithilfe von PHP realisiert, es wird eine    Tabelle mit den Fragen von 1-20 angezeigt.
+			*/
+		?>
+	</aside>
 		 <!-- Hier strukt. Elemente für "Lebensanzahl", "Punktzahl" und "Zeitlimit" einfügen -->
-		
-		<!-- Frage an Herrn Alpers: Welche HTML 5 tags bzw. PHP Funktionen sind sinnvoll, womit ich die genannten Punkte realisieren kann? --> 
+		<?php
+			// Die eingefügten Werte dienen lediglich als Platzhalter
+			$versuche = 3;  	//Variable wird aus Datenbank übernommen 	
+			$punktzahl = 20;  	//Variable wird aus Datenbank übernommen 
+			$zeitlimit =15; 	//Variable wird aus Datenbank übernommen 
+			echo "Versuche: ", $versuche, "<br>"; 
+			echo "Punktzahl: ", $punktzahl, "<br>"; 
+			echo "Zeitlimt: ", $zeitlimit, " sekunden", "<br>"; 	
+		?>
