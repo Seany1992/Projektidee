69f707a4af6ac1354d30d76cb71fe606750bf6e5
Seany1992 <s.gocks@live.de>
2016-10-05 15:51:48 +0200

    Ansicht hub.php hinzugefügt
    Die Ansicht hub hinzugefügt, beninhaltet die include option für die
    Gruppe hauptmenue.php

---
+++ hub.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang=de> 
+	<head>
+		<meta charset=utf-8> 
+		<title>Quizprojekt</title>
+	</head>
+
+	<body>
+		<header>
+			<img><!-- Hier wird das Logo des Quizes eingefügt --></img>
+			<h1>Hauptmenü</h1> 
+		</header>
+		
+		<?php include("Gruppen_de/hauptmenue.php") ?>
+		
+	</body>
+</html>
